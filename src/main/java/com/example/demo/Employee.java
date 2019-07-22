ee3e8d1d046c75dd11f1a4f7e2d3c37252f71fa2
Cedric <cedrichillah@gmail.com>
2019-07-22 19:58:13 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String fName;
+    private String lname;
+    private int SSN;
+    private String DOB;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+}
---
+++ src/main/java/com/example/demo/FormWithBeanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormWithBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormWithBeanApplication.class, args);
+    }
+
+}
